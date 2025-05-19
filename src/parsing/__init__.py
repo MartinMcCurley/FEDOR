f1d4454f46ae73bd7d88ebecec0faf163d07985b
MartinMcCurley <martin.mccurley@gmail.com>
2025-05-19 23:18:16 +0100

    again

--- src/parsing/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Parsing module for GTO chart data.
-
-This module contains utilities for parsing various formats of GTO charts 
-including PDF, HTML, and text-based GTO charts into standardized formats.
-""" 
--- src/parsing/tests/__init__.py
+++
@@ -1,3 +0,0 @@
-"""
-Test module for GTO chart parsing.
-""" 
